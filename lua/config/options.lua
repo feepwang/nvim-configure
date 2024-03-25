76d042d17545cb192f797a9a5842b32d48ce6c4a
feepwang <12470612+feepwang@users.noreply.github.com>
2024-03-25 04:54:38 +0000

    add neovide options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -26,3 +26,7 @@ opt.smartindent = true
 opt.ignorecase = true
 opt.smartcase = true
 
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_scroll_animation_length = 0
+end
